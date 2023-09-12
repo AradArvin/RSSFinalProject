a9941b84a91adc93725118b07f0583c55660924c
arad <aradarvinxo@gmail.com>
2023-09-12 22:26:18 +0330

    Chore: channel serializer

--- RssProject/podcasts/serializers.py
+++ RssProject/podcasts/serializers.py
@@ -11,3 +11,33 @@ class RssFeedSourceSerializer(serializers.ModelSerializer):
             ]
 
 
+
+
+class RssPodcastChannelMetaDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RssPodcastChannelMetaData
+        fields = [
+            'rss_feed', 
+            'title', 
+            'description', 
+            'summary', 
+            'encoded_content', 
+            'subtitle', 
+            'keywords',
+            'language',
+            'category',
+            'link',
+            'author_name',
+            'author_email',
+            'explicit',
+            'image_url',
+            'image_link',
+            'image_title',
+            'podcast_type',
+            'copy_right',
+            'pub_date',
+            'subscribe',
+            ]
+        depth = 2
+
+
