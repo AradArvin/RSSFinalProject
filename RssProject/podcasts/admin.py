72df6ef6a4d627cc5004ac2cc3391b40a6a96ff8
arad <aradarvinxo@gmail.com>
2023-09-12 16:32:35 +0330

    Chore: RssFeedSource model register to admin

--- RssProject/podcasts/admin.py
+++ RssProject/podcasts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import RssFeedSource, RssPodcastChannelMetaData, PodcastEpisodeData
 # Register your models here.
+
+
+@admin.register(RssFeedSource)
+class RssFeedSourceDisplay(admin.ModelAdmin):
+    pass
+
+
