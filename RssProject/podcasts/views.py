3d4450ca77c2f9b3dac632cc3e71e2ad56578f43
arad <aradarvinxo@gmail.com>
2023-09-14 02:55:30 +0330

    Chore: view for save to db

--- RssProject/podcasts/views.py
+++ RssProject/podcasts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .parsers import save_apology_line_feed_to_db
+from django.http import HttpResponse
 # Create your views here.
+
+def save_to_db(request):
+    save_apology_line_feed_to_db()
+    return HttpResponse('OK')
