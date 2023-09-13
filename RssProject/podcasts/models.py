f95740e541616bdeea9d85c9fc5b3caa67feb54b
arad <aradarvinxo@gmail.com>
2023-09-14 02:53:29 +0330

    Refactor: nul and blank added to fields

--- RssProject/podcasts/models.py
+++ RssProject/podcasts/models.py
@@ -15,27 +15,28 @@ class RssPodcastChannelMetaData(models.Model):
     rss_feed = models.ForeignKey(RssFeedSource, on_delete=models.CASCADE)
 
     title = models.CharField(max_length=100)
-
+    
     description = models.TextField()
-    summary = models.TextField()
-    encoded_content = models.TextField()
-    subtitle = models.TextField()
-    keywords = models.TextField()
+    summary = models.TextField(null=True, blank=True)
+    encoded_content = models.TextField(null=True, blank=True)
+    subtitle = models.TextField(null=True, blank=True)
+    keywords = models.TextField(null=True, blank=True)
+    
+    generator = models.CharField(max_length=50, null=True, blank=True)
+    language = models.CharField(max_length=50, null=True, blank=True)
+    category = models.CharField(max_length=50, null=True, blank=True)
+    link = models.CharField(max_length=300, null=True, blank=True)
+    author_name = models.CharField(max_length=100, null=True, blank=True)
+    author_email = models.CharField(max_length=100, null=True, blank=True)    # EmailField
+    explicit = models.CharField(max_length=50, null=True, blank=True)         # BooleanField
+    image_url = models.CharField(max_length=300, null=True, blank=True)       # UrlField
+    image_link = models.CharField(max_length=200, null=True, blank=True)      
+    image_title = models.CharField(max_length=150, null=True, blank=True)      
+    podcast_type = models.CharField(max_length=100, null=True, blank=True)
+    copy_right = models.CharField(max_length=100, null=True, blank=True)
+    pub_date = models.CharField(max_length=100, null=True, blank=True)        # BooleanField
 
-    language = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
-    link = models.CharField(max_length=300)
-    author_name = models.CharField(max_length=100)
-    author_email = models.CharField(max_length=100)    # EmailField
-    explicit = models.CharField(max_length=50)         # BooleanField
-    image_url = models.CharField(max_length=300)       # UrlField
-    image_link = models.CharField(max_length=200)      
-    image_title = models.CharField(max_length=150)      
-    podcast_type = models.CharField(max_length=100)
-    copy_right = models.CharField(max_length=100)
-    pub_date = models.CharField(max_length=100)        # BooleanField
-
-    subscribe = GenericRelation(SubScribe)
+    # subscribe = GenericRelation(SubScribe)
 
 
 class PodcastEpisodeData(models.Model):
