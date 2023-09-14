deca3cd1c08a244215a5bf93cf0aed5683f0064c
arad <aradarvinxo@gmail.com>
2023-09-14 18:56:09 +0330

    Refactor:  unused prints removed

--- RssProject/podcasts/parsers.py
+++ RssProject/podcasts/parsers.py
@@ -4,10 +4,8 @@ from .models import RssFeedSource, RssPodcastChannelMetaData, PodcastEpisodeData
 
 
 fields = RssPodcastChannelMetaData.fields()
-print(fields)
 
 fields2 = PodcastEpisodeData.fields()
-print(fields)
 
 
 
