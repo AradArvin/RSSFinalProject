44f9cebce49bac73e26f247c7f3bf6413d53b47e
arad <aradarvinxo@gmail.com>
2023-09-14 18:36:43 +0330

    Refactor: save view changed to fit new approach

--- RssProject/podcasts/views.py
+++ RssProject/podcasts/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .parsers import save_apology_line_feed_to_db
+from .parsers import save_data_to_db
 from django.http import HttpResponse
 # Create your views here.
 
 def save_to_db(request):
-    save_apology_line_feed_to_db()
+
+    save_data_to_db("com_apology-line","TheApologyLine")
+    save_data_to_db("fm_bibleinayear_rss","TheBibleInAYear")
+    save_data_to_db("com_qm_9xx0g","CrimeJunkie")
+    save_data_to_db("fm_WWO3519750118","TheDanBonginoShow")
+    save_data_to_db("com_54nAGcIl","TheDaily")
+    save_data_to_db("fm_EMPBC2962078635","TheLincolnProject")
+    
     return HttpResponse('OK')
