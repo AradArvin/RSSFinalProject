d2f0abee26c8b56b0766a8317c9cae8f06906794
arad <aradarvinxo@gmail.com>
2023-09-14 18:28:51 +0330

    Chore: func for opennig files

--- RssProject/podcasts/parsers.py
+++ RssProject/podcasts/parsers.py
@@ -11,3 +11,14 @@ print(fields)
 fields2 = PodcastEpisodeData.fields()
 print(fields)
 
+
+
+def file_opener(file_name: str) -> json:
+    with open("../rss_files/urls.json") as urls:
+        url_file = json.load(urls)
+
+    with open("../rss_files/"+file_name+".json") as j_file:
+        json_file = json.load(j_file)
+    
+    return url_file, json_file
+
