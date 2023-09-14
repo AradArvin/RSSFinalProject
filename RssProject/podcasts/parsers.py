0c26bb82565ee86eb2911f343632d8a509a26016
arad <aradarvinxo@gmail.com>
2023-09-14 18:29:38 +0330

    Chore: func for meta model field setter

--- RssProject/podcasts/parsers.py
+++ RssProject/podcasts/parsers.py
@@ -22,3 +22,29 @@ def file_opener(file_name: str) -> json:
     
     return url_file, json_file
 
+
+
+def rssPodcastChannelMetaData_object_field_setter(meta_tags: json, feed_obj: RssFeedSource):
+
+    channel = RssPodcastChannelMetaData.objects.create(
+        rss_feed = feed_obj,
+        title = meta_tags.get("title", None), 
+        description = meta_tags.get("description", None),
+        summary = meta_tags.get("itunes:summary", None),
+        encoded_content = meta_tags.get("content:encoded", None), 
+        subtitle = meta_tags.get("itunes:subtitle", None), 
+        keywords = meta_tags.get("itunes:keywords", None), 
+        generator = meta_tags.get("generator", None),
+        language = meta_tags.get("language", None),
+        category = meta_tags.get("itunes:category", None),
+        link = meta_tags.get("link", None),
+        owner = meta_tags.get("itunes:owner", None),
+        explicit = meta_tags.get("itunes:explicit", None),
+        image_url = meta_tags.get("itunes:image", None),
+        podcast_type = meta_tags.get("itunes:type", None),
+        copy_right = meta_tags.get("copyright", None),
+        pub_date = meta_tags.get("title", None)
+        )
+    return channel
+
+
