b43cbded5963976e1b5cb01d481d180ea2a5abc5
arad <aradarvinxo@gmail.com>
2023-09-15 04:20:20 +0330

    Chore: channel & episode detail view url

--- RssProject/podcasts/urls.py
+++ RssProject/podcasts/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("save/", save_to_db, name="save"),
     path("api/feed/", ListRssSources.as_view(), name="rss_source_api"),
     path("api/channel/", ListRssChannels.as_view(), name="rss_channel_api"),
+    path("api/channel/<int:pk>/", RssChannelDetail.as_view(), name="rss_channel_detail_api"),
     path("api/episodes/", ListRssEpisodes.as_view(), name="rss_episodes_api"),
+    path("api/episodes/<int:pk>/", RssEpisodesDetail.as_view(), name="rss_episodes_detail_api"),
 ]
