748f49a932397f923ff5cc57773ebe3b2fb68646
arad <aradarvinxo@gmail.com>
2023-09-25 21:22:48 +0330

    Chore: custom exception handler

---
+++ RssProject/accounts/exceptions.py
@@ -0,0 +1,19 @@
+from rest_framework.views import exception_handler
+
+
+
+def custom_exception_handler(exc, context):
+
+    response = exception_handler(exc, context)
+    handlers = {
+        'ValidationError': _handle_generic_error
+    }
+
+    exception_class = exc.__class__.__name__
+
+    if exception_class in handlers:
+        response.data['status_code'] = response.status_code
+        return handlers[exception_class](exc, context, response)
+
+    return response
+
