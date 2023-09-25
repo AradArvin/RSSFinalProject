9c8ca8a339e59cb63e07f31b647aba2968f48f53
arad <aradarvinxo@gmail.com>
2023-09-25 22:28:35 +0330

    Chore: login serializer

--- RssProject/accounts/serializers.py
+++ RssProject/accounts/serializers.py
@@ -22,6 +22,56 @@ class RegisterationSerializer(serializers.ModelSerializer):
         return CustomUser.objects.create_user(**validated_data)
 
     
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=512, read_only=True)
+
+
+    def validate(self, attrs):
+        """Validate entered user data and generate refresh and access token for user."""
+
+        email = attrs.get('email', None)
+        password = attrs.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError("An email adress is required to login")
+       
+        if password is None:
+            raise serializers.ValidationError("A password is required to login")
+        
+
+        user = authenticate(username=email, password=password)
+        
+
+        if user is None:
+            raise serializers.ValidationError("User not found! Please check your email or password.")
+        
+
+        if check_cache(user.id):
+            raise serializers.ValidationError("The user is already logged in!") 
+
+
+        if not user.is_active:
+            raise serializers.ValidationError("This user has been deavtivated")
+        
+        access_token = access_token_gen(user.pk)
+        refresh_token = refresh_token_gen(user.pk)
+
+        cache_setter(refresh_token)
+
+        validated_data = {
+            'email': user.email,
+            'username': user.username,
+            'token': access_token
+        }
+    
+        return validated_data
+    
+
+
     class Meta:
         pass
 
