0b249519db82190553cc18a7ccd70c4b17b240b7
arad <aradarvinxo@gmail.com>
2023-09-25 22:31:29 +0330

    Chore: func's to generate access & refresh token

--- RssProject/accounts/utils.py
+++ RssProject/accounts/utils.py
@@ -19,3 +19,33 @@ class TokenNotFound(APIException):
     default_code = '403'
 
 
+
+def access_token_gen(user_id: int):
+    """Generate access token based on usser id."""
+
+    access_token = token_encode({
+        'token_type':'access',
+        'user_id':user_id,
+        'exp': datetime.utcnow() + timedelta(minutes=1),
+        'iat': datetime.utcnow(),
+        'jti':gen_jti()
+    })
+
+    return access_token
+
+
+
+def refresh_token_gen(user_id: int):
+    """Generate refresh token based on usser id."""
+
+    refresh_token = token_encode({
+        'token_type':'refresh',
+        'user_id':user_id,
+        'exp': datetime.utcnow() + timedelta(days=1),
+        'iat': datetime.utcnow(),
+        'jti':gen_jti()
+    })
+    
+    return refresh_token
+
+
