f684d84f5667270a52889a4e39beadb9ec1dc97f
arad <aradarvinxo@gmail.com>
2023-10-03 23:37:16 +0330

    Refactor: changed retry mechanizm

--- RssProject/podcasts/tasks.py
+++ RssProject/podcasts/tasks.py
@@ -2,17 +2,11 @@ from celery import Task
 from celery import shared_task
 from .parsers import save_data_to_db
 
-import logging
-logger = logging.getLogger('celery')
-
 
 
 class BaseTaskWithRetry(Task):
-    autoretry_for = (Exception,)
     retry_backoff = 2
     retry_jitter=True
-    retry_kwargs = {'max_retries': 5}
-
 
 
 
@@ -20,7 +14,5 @@ class BaseTaskWithRetry(Task):
 def task_rss_parsing(self, link):
     try:
         save_data_to_db(rss_link=link)
-        logger.info('Parsing started...')
     except Exception as e:
-        logger.error(e)
-        raise Exception(e)
+        raise self.retry(exc=e, countdown=5)
