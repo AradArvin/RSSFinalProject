96aa8a9db346798cfd172efd35dd8effca1dbd52
arad <aradarvinxo@gmail.com>
2023-10-04 22:56:19 +0330

    Chore: like api view

--- RssProject/interactions/views.py
+++ RssProject/interactions/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
+from .models import *
+from .serializers import *
+from podcasts.serializers import PodcastEpisodeListSerializer
+from .utils import podcast_recommendations
+
 
 # Create your views here.
+
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 1000
+
+
+
+
+class LikeAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = LikeSerializer
+
+    def post(self, request, episode_id):
+        episode = PodcastEpisodeData.objects.get(id=episode_id)
+        like, created = Like.objects.get_or_create(user=request.user, episode=episode)
+
+        if created:
+            serializer = self.serializer_class(like)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            msg = {'status':'This episode is already liked!'}
+            return Response(msg, status=status.HTTP_200_OK)
+        
+    
+    def delete(self, request, episode_id):
+        episode = PodcastEpisodeData.objects.get(id=episode_id)
+        try:
+            like = Like.objects.get(user=request.user, episode=episode)
+            like.delete()
+            msg = {'status':'Unliked!'}
+            return Response(msg, status=status.HTTP_204_NO_CONTENT)
+        except Like.DoesNotExist:
+            msg = {'status':'This episode is not liked!'}
+            return Response(msg, status=status.HTTP_404_NOT_FOUND)
+        
+
+
