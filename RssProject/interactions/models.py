da8d7933070153ff3fa143c7f9fd7be33fa86e11
arad <aradarvinxo@gmail.com>
2023-10-04 22:54:38 +0330

    Refactor: models for interactions changed

--- RssProject/interactions/models.py
+++ RssProject/interactions/models.py
@@ -1,40 +1,65 @@
 from django.db import models
 from accounts.models import CustomUser
-from podcasts.models import *
+from podcasts.models import PodcastEpisodeData
 
 # Create your models here.
 
 
 
 class Comment(models.Model):
-    opinion = models.TextField()
-    commenter = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-
-
+    content = models.TextField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    episode = models.ForeignKey(PodcastEpisodeData, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_accepted = models.BooleanField(default=True)
 
     def __str__(self) -> str:
-        return self.opinion[:15]
+        return f"This comment was left by {self.user.username} for {self.episode.title}"
 
 
 
 
 class Like(models.Model):
-    users = models.ManyToManyField(CustomUser)
-
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    episode = models.ForeignKey(PodcastEpisodeData, on_delete=models.CASCADE)
 
 
     def __str__(self) -> str:
-        return self.likes.count()
-
-
-
-class SubScribe(models.Model):
-    subscribed = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+        return f"{self.user.username} liked {self.episode.title}"
 
 
 
 
 
 class BookMark(models.Model):
-    bookmarked = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    episode = models.ForeignKey(PodcastEpisodeData, on_delete=models.CASCADE)
 
+
+    def __str__(self) -> str:
+        return f"Bookmark saved by {self.user.username}"
+
+
+
+
+class ViewdPodcasts(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    episode = models.ForeignKey(PodcastEpisodeData, on_delete=models.CASCADE)
+    viewed_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return f"Viewd by {self.user.username}"
+
+
+
+
+
+
+
+
+
+
+class SubScribe(models.Model):# Unused for now.
+    subscribed = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
