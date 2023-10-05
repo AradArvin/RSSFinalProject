bf55337eddffa43dde83b795ab925cc8b3210a04
arad <aradarvinxo@gmail.com>
2023-10-05 17:01:18 +0330

    Chore: urls for forget and set password

--- RssProject/accounts/urls.py
+++ RssProject/accounts/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('api/login', LoginAPIView.as_view(), name='login'),
     path('api/logout', LogOutAPIView.as_view(), name='logout'),
     path('api/user', UserRetrieveUpdateAPIView.as_view(), name='user'),
-    path('api/changepass', ChangePasswordView.as_view(), name='change_password'),
+    path('api/change-pass', ChangePasswordView.as_view(), name='change_password'),
+    path('api/forget-pass', ForgetPasswordView.as_view(), name='forget_password'),
+    path('api/set-pass/<str:hex>', SetNewPasswordView.as_view(), name='set_password'),
     path('api/refresh', RefreshTokenAPIView.as_view(), name='refresh'),
     path('api/access', AccessTokenAPIView.as_view(), name='access'),
 ]
