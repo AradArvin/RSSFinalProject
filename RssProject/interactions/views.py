7866c1673cca4d110fe01403a28029654c9064a2
arad <aradarvinxo@gmail.com>
2023-10-06 10:15:34 +0330

    Feat: custom pagination furthur customized

--- RssProject/interactions/views.py
+++ RssProject/interactions/views.py
@@ -16,7 +16,18 @@ from .utils import podcast_recommendations
 class CustomPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
-    max_page_size = 1000
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "results": data,
+            }
+        )
 
 
 
