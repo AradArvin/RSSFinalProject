060cc7d2b08e70f0c4f481769a571ecc4da19a16
arad <aradarvinxo@gmail.com>
2023-10-16 15:09:59 +0330

    Fix: try except block deleted

--- RssProject/podcasts/tasks.py
+++ RssProject/podcasts/tasks.py
@@ -13,6 +13,8 @@ logger = logging.getLogger("elastic_logger")
 from .parsers import save_data_to_db
 
 
+
+
 class BaseTaskWithRetry(Task):
     autoretry_for = (Exception,)
     retry_kwargs = {'max_retries': 5}
@@ -66,7 +68,6 @@ class BaseTaskWithRetry(Task):
 
 @shared_task(bind=True, base=BaseTaskWithRetry)
 def task_rss_parsing(self, link):
-    try:
-        save_data_to_db(rss_link=link)
-    except Exception as e:
-        raise self.retry(exc=e, countdown=5)
+    
+    save_data_to_db(rss_link=link)
+    
