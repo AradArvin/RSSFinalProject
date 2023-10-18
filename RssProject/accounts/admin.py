aaf8ff0e4d33b2d2ea02e5f326ae726dc4c7f296
arad <aradarvinxo@gmail.com>
2023-10-18 13:07:30 +0330

    Chore: register new models to admin

--- RssProject/accounts/admin.py
+++ RssProject/accounts/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import *
 # Register your models here.
 
 
 @admin.register(CustomUser)
 class CustomUserDisplay(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Notif)
+class NotifDisplay(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserNotif)
+class UserNotifDisplay(admin.ModelAdmin):
     pass
